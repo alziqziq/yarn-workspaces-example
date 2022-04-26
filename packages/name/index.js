fc67187e6590fb525a95e7180ee9334d1ee932ba
alziqziq <alziqziq@gmail.com>
2022-04-26 13:27:10 +0700

    add name

---
+++ packages/name/index.js
@@ -0,0 +1,8 @@
+function getName() {
+  console.log('ALAL === Muhamad Al Ziqri', {  });
+  console.log('ALAL === Muhamad Al Ziqri', {  });
+  console.log('ALAL === Muhamad Al Ziqri', {  });
+  console.log('ALAL === Muhamad Al Ziqri', {  });
+}
+
+module.exports = getName; 
